7f2b27a1321fdeb987e78d856ba5a1cbf67f80e6
Gavin King <gavin@vimeo.com>
2016-10-22 16:39:05 -0400

    cleanup

--- index.js
+++ index.js
@@ -13,16 +13,32 @@ app.get('/', function(req, res){
 	res.sendFile('html/index.html', {root: __dirname })
 });
 
+http.listen(3000, function(){
+  console.log('Listening at http://localhost:3000');
+});
+
 io.on('connection', function(socket){
+	
+	startEmittingModules(socket)
+});
+
+io.on('disconnect', function(socket){
+
+	stopEmittingModules(socket)
+});
+
+function startEmittingModules(socket)
+{
     modules = fs.readdirSync(modulesPath);
     i = 0;
     setInterval(function() {
         module = modulesPath + '/' + modules[i];
-        io.emit('src', module);
+        socket.emit('src', module);
         i = (i + 1) % modules.length;
-    }, 5000);
-});
+    }, 1000);
+}
 
-http.listen(3000, function(){
-  console.log('Listening at http://localhost:3000');
-});
+function stopEmittingModules(socket)
+{
+	clearInterval()
+}
