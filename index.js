8441999bd2629e8272d9e559142779ae3d76e933
Gavin King <gavin@vimeo.com>
2016-10-22 15:45:38 -0400

    connecting

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use('/js', express.static(__dirname + '/js'));
 
-app.get('/', function(req, res) {
-    res.sendFile('html/index.html', {root: __dirname })
+app.get('/', function(req, res){
+	res.sendFile('html/index.html', {root: __dirname })
 });
 
-io.on('connection', function(){
-    console.log("we connected yo");
+io.on('connection', function(socket){
+  console.log('a user connected');
 });
 
-server.listen(3000);
-
+http.listen(3000, function(){
+  console.log('Listening at http://localhost:3000');
+});
--- js/index.js
+++ js/index.js
@@ -1,11 +1,1 @@
-var socket = io.connect('http://localhost');
-socket.on('connect', function(){
-    console.log('connected');
-});
-socket.on('event', function(data){
-    console.log('event');
-});
-socket.on('disconnect', function(){
-    console.log('disconnect');
-});
-
+var socket = io();
